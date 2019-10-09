6564075c7050d1278536c785466b428ea884f3aa
Felipe Romero <feliperomero3@outlook.com>
2019-10-08 22:26:16 -0500

    Update Main.

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore;
+﻿using System.Data.SqlClient;
+using ContosoUniversity.Data;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace ContosoUniversity
 {
@@ -7,7 +11,29 @@ namespace ContosoUniversity
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+
+                    context.Database.EnsureCreated();
+                }
+                catch (SqlException ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+
+                    logger.LogError(ex, "An error occurred creating the DB.");
+
+                    throw;
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
