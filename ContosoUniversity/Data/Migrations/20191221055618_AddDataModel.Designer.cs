634069a71630f17b2eb703969e6ee935d403085e
Felipe Romero <feliperomero3@outlook.com>
2020-01-15 22:54:21 -0600

    Add AddDataModel database migration.

--- ContosoUniversity/Data/Migrations/20191221055618_AddDataModel.Designer.cs
+++ ContosoUniversity/Data/Migrations/20191221055618_AddDataModel.Designer.cs
@@ -4,14 +4,16 @@ using ContosoUniversity.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ContosoUniversity.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191221055618_AddDataModel")]
+    partial class AddDataModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
