d3d5ed48edf7db3371c20a814a853ace603ef97c
Felipe Romero <feliperomero3@outlook.com>
2020-01-17 01:08:55 -0600

    Unit test Details page.

---
+++ ContosoUniversity.Tests/Pages/Students/DetailsModelTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using ContosoUniversity.Data;
+using ContosoUniversity.Entities;
+using ContosoUniversity.Pages.Students;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Moq;
+using Xunit;
+
+namespace ContosoUniversity.Tests.Pages.Students
+{
+    public class DetailsModelTest
+    {
+        [Fact]
+        public async void Gets_a_Student_Details()
+        {
+            // Arrange
+            var mockRepository = new Mock<IStudentRepository>();
+
+            mockRepository.Setup(r => r.GetStudentDetailAsync(It.IsAny<int>()))
+                .Returns(Task.FromResult(new Student()));
+
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+
+            mockUnitOfWork.SetupGet(u => u.Student).Returns(mockRepository.Object);
+
+            var target = new DetailsModel(mockUnitOfWork.Object);
+
+            // Act
+            var result = await target.OnGetAsync(It.IsAny<int>());
+
+            // Assert
+            Assert.IsType<PageResult>(result);
+            Assert.NotNull(target.Student);
+            mockUnitOfWork.Verify(u => u.Student.GetStudentDetailAsync(It.IsAny<int>()), Times.Once);
+        }
+    }
+}
